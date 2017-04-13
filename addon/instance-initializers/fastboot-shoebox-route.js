19fc25aed4962e8d74776700637a482d949bca85
Adam Baker <bakerac4@gmail.com>
2017-04-13 13:30:00 -0400

    Creating Version 1.0 candidate - completely revamped transfer of data

--- addon/instance-initializers/fastboot-shoebox-route.js
+++ addon/instance-initializers/fastboot-shoebox-route.js
@@ -1,37 +1,27 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 const {
     computed,
     get,
     set,
-    setProperties,
     String: { dasherize },
     isArray,
-    $,
-    keys: emberKeys
+    isEmpty,
+    keys: emberKeys,
+    on,
+    run: { scheduleOnce }
 } = Ember;
 
 const keys = Object.keys || emberKeys;
 
 export function initialize(applicationInstance) {
     let fastboot = applicationInstance.lookup('service:fastboot');
-    let shoeboxSerializer = applicationInstance.lookup('serializer:shoebox');
     let store = applicationInstance.lookup('service:store');
-    if (shoeboxSerializer) {
-        set(shoeboxSerializer, 'store', store);
-    }
 
     Ember.Route.reopen({
-        fastboot: fastboot,
-
-        /**
-            Tells the route if the data for this specific route has been loaded from the shoebox already.
-            @property dataLoadedFromShoebox
-            @type Boolean
-            @default false
-            @public
-        */
-        dataLoadedFromShoebox: false,
+        shoeboxRouteEnabled: true,
+        fastboot,
 
         /**
             This property becomes the shoebox store name when serialized.
@@ -66,7 +56,7 @@ export function initialize(applicationInstance) {
             @type Boolean
             @public
         */
-        shouldLoadFromShoebox: computed('fastboot.isFastBoot', 'fastboot.shoebox', 'dataLoadedFromShoebox', function() {
+        shouldLoadFromShoebox: computed('fastboot.isFastBoot', 'fastboot.shoebox', function() {
             if (get(this, 'fastboot.isFastBoot')) {
                     return false;
             } else {
@@ -80,77 +70,104 @@ export function initialize(applicationInstance) {
         }),
 
         /**
-            Serializes the shoebox data for a particular model
-            @method serializeShoeboxData
-            @param {DS.Model} model The model data of the route
-            @return {Object} The serialized model data
-            @public
-        */
-        serializeShoeboxData (model) {
-            if(isArray(model)) {
-                return model.reduce((a,b) => a.concat(b), [])
-                    .map(record => record._createSnapshot())
-                    .map(snapshot => shoeboxSerializer.serialize(snapshot, { includeId: true }))
-                    .reduce((a,b) => { a.data.push(b.data); return a; }, { data: [] });
-            } else {
-                return shoeboxSerializer.serialize(model._createSnapshot(), { includeId: true });
-            }
-        },
+            Given a ED model, this method serializes it by looking up the applications serializers
+            for the specific model (which fall back to the global application serializer if neccessary)
+            Then it uses the store to normalize the data into the JSON API standard so it can be easily
+            pushed back into the store when the web application boots up.
 
-        /**
-            Unescapes the html
-            @method unescapeHtml
-            @param {String} safe A JSON string representing the shoebox store
-            @return {String}
+            @method serializeEDModel
+            @param {DS.Model|DS.RecordArray} model The model data of the route
+            @return {Object} The normalized model data
             @public
         */
-        unescapeHtml(safe) {
-            return $('<div />').html(safe).text();
+        serializeEDModel (model) {
+            if(isArray(model)) {
+                const modelName = model.get('firstObject.constructor.modelName');
+                const serializer = store.serializerFor(modelName);
+                const json = model.reduce((a,b) => a.concat(b), [])
+                    .map(record => record._createSnapshot())
+                    .map(snapshot => {
+                      let json = serializer.serialize(snapshot, { includeId: true });
+                      let normalizedJson = store.normalize(modelName, json);
+                      normalizedJson.__emberDataModel = true;
+                      return normalizedJson;
+                  });
+                return json;
+            } else {
+                const modelName = model.constructor.modelName;
+                const serializer = store.serializerFor(modelName);
+                const json = serializer.serialize(model._createSnapshot(), { includeId: true });
+                let normalizedJson = store.normalize(modelName, json);
+                normalizedJson.__emberDataModel = true;
+                return normalizedJson;
+            }
         },
 
         /**
             Parses the shoebox store information for the route and returns the records that were pushed into the ember data store
             @method getShoeboxModelForRoute
-            @return {DS.Model|Array} The record(s) that were pushed into the DS store
+            @return {DS.Model|DS.Array|Object} The record(s) that were pushed into the DS store
             @public
         */
         getShoeboxModelForRoute() {
             let shoebox = get(fastboot, 'shoebox');
             let routeName = get(this, 'routeNameDasherized');
             let records = null;
-            let isSingleRecord = false;
             let shoeboxStore = this.getShoeboxStore(routeName);
 
             if (!shoeboxStore) {
                 return;
             }
 
-            //There should only be one key in the object
-            keys(shoeboxStore).forEach((key) => {
-                set(this, 'modelName', key);
-                //We need to unescape the html until https://github.com/ember-fastboot/fastboot/pull/79
-                let value = this.unescapeHtml(JSON.stringify(shoeboxStore[key]));
-                let deserializedData = JSON.parse(value);
-                isSingleRecord = !isArray(deserializedData.data);
+            records = this.deserializeShoeboxModel(shoeboxStore);
+            this.clearShoeboxRoute(shoebox, routeName);
+            return records;
+        },
 
-                if(isSingleRecord) {
-                    deserializedData.data = Ember.A([deserializedData.data]);
+        /**
+            Recusively deserializes the data for a particular shoebox
+            @method deserializeShoeboxModel
+            @param {Object} model The object given from the shoebox
+            @return {DS.Model|DS.Array|Object} The actual route model
+            @public
+        */
+        deserializeShoeboxModel(model) {
+            if (!isEmpty(model)) {
+                if (this.isEDModel(model)) {
+                    delete model.__emberDataModel;
+                    return store.push(model);
                 }
 
-                records = store.push(deserializedData);
-            });
+                if (typeof model === 'object') {
+                    keys(model).forEach(key => {
+                        let attribute = model[key];
+                        let attributeModel = this.deserializeShoeboxModel(attribute);
+                        model[key] = attributeModel;
+                        return attributeModel;
+                    });
+                }
+            }
 
+            return model;
+        },
+
+        /**
+            Removes specific shoebox from DOM
+            @method clearShoeboxRoute
+            @param {Object} shoebox The shoebox object
+            @param {String} routeName The name of the route to clear the shoebox for
+            @public
+        */
+        clearShoeboxRoute (shoebox, routeName) {
             set(shoebox, routeName, null);
             let shoeboxStoreNode = document.querySelector(`#shoebox-${routeName}`);
             shoeboxStoreNode.parentElement.removeChild(shoeboxStoreNode);
-
-            return isSingleRecord ? records.get('firstObject') : records;
         },
 
         /**
             The method thats get called inplace of the user defined model
             @method shoeboxModel
-            @return {DS.Model|Array} The record(s) that were in the shoebox store
+            @return {DS.Model|Array|Object} The record(s) that were in the shoebox store
             @public
         */
         shoeboxModel() {
@@ -160,25 +177,18 @@ export function initialize(applicationInstance) {
         /**
             Checks to see if we should load the shoebox data.
 
-            If we should it saves the routes user defined model method as originalModel and marks dataLoadedFromShoebox as true.
+            If we should it saves the routes user defined model method as originalModel.
             It then sets the shoebox model as the routes current model.
 
             If we should not load from shoebox & an orignalModel exists we want to set originalModel back to the model so the application will function correctly on next load of route.
 
-            NOTE: You must call this._super on any beforeModel you define in order for this to run
-
-            @method beforeModel
-            @public
+            @method selectModel
+            @private
         */
-        beforeModel() {
+        _selectModel () {
             if (get(this, 'shouldLoadFromShoebox')) {
-                setProperties(this, {
-                    originalModel: get(this, 'model'),
-                    dataLoadedFromShoebox: true
-                });
+                set(this, 'originalModel', get(this, 'model'));
                 set(this, 'model', this.shoeboxModel);
-            } else if (get(this, 'originalModel')) {
-                set(this, 'model', this.originalModel);
             }
         },
 
@@ -186,29 +196,82 @@ export function initialize(applicationInstance) {
             Checks to see if we are in fastboot, and if the route has model.
             If thats the case we want to serialize the model and add it to the shoebox
 
-            NOTE: You must call this._super on any afterModel you define in order for this to run
-
-            @method afterModel
-            @param {DS.Model} model
-            @public
+            @method serializeRouteModel
+            @private
         */
-        afterModel(model) {
+
+        _serializeRouteModel (){
             let routeName = get(this, 'routeNameDasherized');
             let shoebox = get(fastboot, 'shoebox');
-            let shoeboxStore = this.getShoeboxStore(routeName);
+            let model = this.currentModel;
             //If this is in fastboot, put this routes model into a specific shoebox store so we can transfer exactly what was in the model to the correct route
-            if (get(fastboot, 'isFastBoot') && model) {
-                if (!shoeboxStore) {
-                    shoeboxStore = {};
-                    shoebox.put(routeName, shoeboxStore);
-                }
-                shoeboxStore[get(this, 'modelName')] = this.serializeShoeboxData(model);
+            if (get(fastboot, 'isFastBoot') && model && get(this, 'shoeboxRouteEnabled')) {
+                let data = this.serializeShoeboxModel(model);
+                shoebox.put(routeName, data);
             }
+
+            if (get(this, 'originalModel')) {
+                set(this, 'model', get(this, 'originalModel'));
+            }
+
         },
+
+        /**
+            Checks if an object is an ED (Ember Data) Model. First checks the instanceof to see if the item is
+            an instance of a DS object. If the item is not a instance of a DS object, and the item is an object
+            we check to see if it has __emberDataModel as true. If thats the case, this is a ED model which has
+            just been serialized already.
+
+            @method isEDModel
+            @param {Object} model
+            @return {Boolean}
+            @public
+        */
+        isEDModel(item) {
+            let isEDInstance = (item instanceof DS.RecordArray || item instanceof DS.Model);
+            if (!isEDInstance && typeof item === 'object'){
+                isEDInstance = item.__emberDataModel || false;
+            }
+            return isEDInstance;
+        },
+
+        /**
+            Recursively serializes the shoebox data for a particular object (Ember Data or not)
+
+            @method serializeShoeboxModel
+            @param {Object} model
+            @return {Object}
+            @public
+        */
+        serializeShoeboxModel (model) {
+            if (!isEmpty(model)) {
+                if (this.isEDModel(model)) {
+                    let json = this.serializeEDModel(model);
+                    return json;
+                }
+                if (typeof model === 'object') {
+                    keys(model).forEach(key => {
+                        let attribute = model[key];
+                        let json = this.serializeShoeboxModel(attribute);
+                        model[key] = json;
+                        return json;
+                    });
+                }
+            } else {
+                return null;
+            }
+            return model;
+        },
+        _activate: on('activate', function() {
+            scheduleOnce('routerTransitions', this, this._serializeRouteModel);
+        }),
+        _onInit: on('init', function() {
+            scheduleOnce('sync', this, this._selectModel);
+        })
     });
 }
 
 export default {
   name: 'fastboot-data-route',
-  initialize: initialize
+  initialize
 };
